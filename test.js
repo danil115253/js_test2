66018b68600d11fd270f0204ae734248ba6b2fc6
unknown <danil115253@gmail.com>
2021-10-01 15:12:19 +0300

    js_test2

---
+++ test.js
@@ -0,0 +1,98 @@
+// let age = 91;
+
+// if (!(age >= 14 && age <=90)) {
+// console.log( "v ne diapazone" )
+// }
+//     else { 
+//         console.log (" v diapazone ")
+//     }
+
+//
+
+// let a = 3, b = 4;
+
+// let result = ( a + b < 4) ? true  : false ;
+
+// console.log (result);
+
+// alert("i am alert")
+
+
+//
+
+// let value  = prompt ('message');
+// console.log(value);
+// console.log (+value + 2)
+
+//
+
+// let str = prompt ("kto tam?");
+// console.log (str);
+
+// switch (str) {
+//     case null:
+//         alert('отменено');
+//         break;
+
+//     case "Админ":
+//       let password = prompt ("password");
+//         switch(password) {
+//             case null:
+//                 alert("Отменено");
+//                 break;
+
+//             case ("Я главный"):
+//                 alert("Здравствуйте")
+//                 break;
+//             default:
+
+//                 alert("неверный пароль");
+
+//         }
+//         break;
+//     default:
+//         alert("Я вас не знаю")
+// }
+
+//
+
+/*alert("Privet Java Script");
+alert("Hello")*/
+
+//  
+
+                // Встраивание с помошью Prompt - возваращает ответ пользователя.
+
+// let age = prompt ("Сколько тебе лет?", 100);
+// alert (`Тебе ${age} лет`);
+
+              // Если нажать от то выведет True / отмена false 
+
+// let isBoss = confirm("Ты здесь главный?");
+// alert (isBoss);
+
+/* Задачка
+
+let userName = prompt("Как тебя зовут?");
+console.log (`Тебя зовут ${userName}`); */
+
+
+
+// Д\З
+
+//  let num = 5;
+//  let isSimpl = true;
+
+//  for (let i =2; i <= num - 1; i++) {
+//      if (num % i === 0 ) {
+//          isSimpl = false;
+//          break;
+//      }
+//  }
+
+
+
+
+   
+
+ 
--- test2.js
+++ test2.js
@@ -1,1 +1,54 @@
-fos tbsp rt tus rt
+  // var x = 0;
+  // if (x == 0)
+  //   console.log ("Work");
+  //   for (var i = 0; i < 5; i++)
+  //   console.log(i);
+
+  //   setTimeout(function () {
+  //     console.log("Привет");
+  //   }, 2000);
+
+  let count2 = 15;
+
+  switch (count2) {
+    case 14:
+    case 13:
+    case 15:
+      console.log("14 || 13 || 15");
+      break;
+  }
+
+// let counter = 0;
+// while (true) {
+//   console.log("1");
+//   counter++ ;
+
+//   if (counter === 5) {
+//     break;
+//   } 
+// }
+
+//
+
+// let counter2 = 0;
+// do {
+//   console.log ("duing");
+//   counter2++;
+// }
+//     while (counter2 < 5)
+
+// let counter = 2;
+// do {
+//   console.log (counter);
+// } while (counter !== 2);
+
+ let counter = 0;
+ for (;;) {
+   counter++;
+   console.log("foring");
+     if (counter === 5) {
+      break;
+     }
+ }
+
+
